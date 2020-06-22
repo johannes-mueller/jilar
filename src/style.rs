6c02055c8ab5a4e0d11f7cb3ec4801afb020538d
Johannes Mueller <github@johannes-mueller.org>
2020-06-22 16:36:30 +0200

    Make things a little bigger

--- src/style.rs
+++ src/style.rs
@@ -1,10 +1,10 @@
 
-pub const BUTTONFONT: &str = "Sans 10px";
+pub const BUTTONFONT: &str = "Sans 12px";
 
 pub const WIDGET_COLOR_SAT: f64 = 0.3;
 pub const BRIGHTNESS_NORMAL: f64 = 0.4;
 pub const BRIGHTNESS_HOVER: f64 = 0.6;
 
-pub const DIAL_DIAMETER: f64 = 24.0;
+pub const DIAL_DIAMETER: f64 = 36.0;
 
-pub const LED_DIAMETER: f64 = 8.0;
+pub const LED_DIAMETER: f64 = 12.0;
